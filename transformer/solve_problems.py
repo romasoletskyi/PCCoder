40995f90c760aeda247db7d456623a8a99be2bec
Roman Soletskyi <romansoletskyi@gmail.com>
2022-04-27 14:44:22 +0300

    sampling bugs

--- transformer/solve_problems.py
+++ transformer/solve_problems.py
@@ -48,8 +48,26 @@ def solve_problems(problems, model, timeout, max_program_len, num_workers):
         return [solve_problem_worker(data) for data in problems]
 
 
+def choose_top(probs, top_p):
+    """
+    Args:
+        probs: Tensor, shape [batch_size, hid]
+        top_p:
+
+    Returns:
+
+    """
+    indices = torch.arange(probs.shape[1])
+    bound_indices = torch.argmax((torch.cumsum(probs, 1) > top_p).to(torch.long), 1)
+
+    mask = indices[None, :] <= bound_indices[:, None]
+    probs *= mask
+    probs /= probs.sum(1)[:, None]
+
+    return probs
+
 def sampling(start_env, max_program_len, model, timeout, top_p, temperature):
-    sample_size = 512
+    sample_size = 64
     start_time = time.time()
     end_time = start_time + timeout
 
@@ -57,7 +75,6 @@ def sampling(start_env, max_program_len, model, timeout, top_p, temperature):
     invalid_steps = 0
 
     batch_indices = torch.arange(sample_size)[:, None]
-
     while time.time() < end_time:
         envs = [start_env.copy() for _ in range(sample_size)]
         statements = [[] for _ in range(sample_size)]
@@ -68,23 +85,24 @@ def sampling(start_env, max_program_len, model, timeout, top_p, temperature):
                     return {'result': statement_list, 'num_steps': num_steps, 'time': time.time() - start_time,
                             'num_invalid': invalid_steps}
 
-            if time.time() > end_time:
+            """if time.time() > end_time:
                 return {'result': False, 'num_steps': num_steps, 'time': time.time() - start_time,
-                        'num_invalid': invalid_steps}
+                        'num_invalid': invalid_steps}"""
 
             env_encodings = torch.tensor(np.array([env.get_encoding() for env in envs]))
             num_inputs = torch.tensor(np.array([env.states[0].num_inputs for env in envs]))
             num_vars = torch.tensor(np.array([env.num_vars for env in envs]))
             statement_pred, statement_log_probs, drop_indx = model.predict(env_encodings, num_inputs, num_vars)
+            statement_pred = torch.flip(torch.tensor(statement_pred), (1,))
 
-            statement_probs = torch.exp(statement_log_probs / temperature)
-            statement_probs *= torch.cumsum(statement_log_probs[batch_indices, statement_pred], 1) <= top_p
+            statement_probs = torch.exp(statement_log_probs[batch_indices, statement_pred] / temperature)
             statement_probs /= torch.sum(statement_probs, 1)[:, None]
-            next_statement = torch.multinomial(statement_probs, 1).squeeze().detach().cpu().numpy()
+            statement_probs = choose_top(statement_probs, top_p)
+            next_statement = statement_pred[batch_indices, torch.multinomial(statement_probs, 1)].squeeze().detach().numpy()
 
             for i, env in enumerate(envs):
                 envs[i] = env.step_safe(index_to_statement[next_statement[i]])
-                statements[i].append(next_statement[i])
+                statements[i].append(index_to_statement[next_statement[i]])
 
                 num_steps += 1
                 if envs[i] is None:
@@ -92,6 +110,9 @@ def sampling(start_env, max_program_len, model, timeout, top_p, temperature):
 
             statements = [statement_list for env, statement_list in zip(envs, statements) if env is not None]
             envs = [env for env in envs if env is not None]
+
+            if not envs:
+                break
 
             repeat_indices = np.random.randint(len(envs), size=sample_size - len(envs))
             envs += [envs[i].copy() for i in repeat_indices]
@@ -105,7 +126,7 @@ def solve_problem_worker(data):
     examples = Example.from_line(data)
     env = ProgramEnv(examples)
 
-    solution = sampling(env, max_program_len, model, timeout, top_p=0.8, temperature=1)
+    solution = sampling(env, max_program_len, model, timeout, top_p=0.9, temperature=0.5)
 
     counter.value += 1
     print("\rSolving problems... %d (failed: %d)" % (counter.value, fail_counter.value), end="")
